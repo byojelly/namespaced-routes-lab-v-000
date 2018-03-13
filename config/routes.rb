391e0b2edcb2d987bf2c42c31c8e7a736e504521
Alex <github email address>
2018-03-13 10:36:22 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+      #nested routes
+      resources :artists do
+        resources :songs, only: [:index, :show]
+      end
+      resources :songs
 
-  resources :artists do
-    resources :songs, only: [:index, :show]
-  end
-  resources :songs
-
+      #namespaced routes to organize
+      namespace :admin do
+        resources :preferences, only: [:index]
+      end
 end
