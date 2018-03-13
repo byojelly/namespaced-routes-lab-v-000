435d47288a12dc36ee18c9006afe734d0d5067ff
Alex <github email address>
2018-03-13 12:15:32 -0400

    Done.

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -5,6 +5,7 @@ class Admin::PreferencesController < ApplicationController
   end
 
   def update
+    binding.pry
     @preference = Preference.find(params[:id])
     @preference.update(preference_params)
     redirect_to admin_preferences_path
