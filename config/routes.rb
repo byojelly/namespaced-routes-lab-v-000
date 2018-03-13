4ee677a33295a9a59903cce65f46d40022dc6c5b
Alex <github email address>
2018-03-13 12:14:35 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
       #namespaced routes to organize
       #this is an admin module to seperate its functionality from the user functionality
       namespace :admin do
-        resources :preferences, only: [:index]
+        resources :preferences, only: [:index, :update]
       end
 end
